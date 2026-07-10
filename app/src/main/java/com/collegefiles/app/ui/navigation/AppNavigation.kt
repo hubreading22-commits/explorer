91b4a2036eb32b3611fb52b29f464c6d51ce5928
hubreading22-commits <hubreading22gmail.com>
2026-07-10 12:21:26 +0530

    Fix unresolved reference AppModule in AppNavigation

--- app/src/main/java/com/collegefiles/app/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/collegefiles/app/ui/navigation/AppNavigation.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.collegefiles.app.di.AppModule
 import com.collegefiles.app.ui.explorer.ExplorerScreen
 import com.collegefiles.app.ui.explorer.ExplorerViewModel
 import com.collegefiles.app.ui.explorer.FileOpsViewModel
